0d769121cef2d1b93c8592490a72eab3af500ae1
rostamali <rostamsardar446@gmail.com>
2022-02-25 23:06:15 +0600

    add stripe code with flash deal collection

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.json());
 
 require('dotenv').config()
 
+// STRIPE CONNECTION
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET}`);
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kniae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -20,20 +23,41 @@ async function run() {
     try {
         await client.connect();
         const database = client.db("eStore");
-        const productsCollection = database.collection("products");
+        const shopCollection = database.collection("products");
+        const flashDealCollection = database.collection("flashDeal");
 
         // get products
         app.get('/products', async (req, res)=>{
-            const products = productsCollection.find({});
+            const products = shopCollection.find({});
             const result = await products.toArray();
             res.send(result);
         });
 
         // get flash deal product
         app.get('/flashdeal', async (req, res)=>{
-            const products = productsCollection.find({}).limit(5);
+            const products = flashDealCollection.find({});
             const result = await products.toArray();
             res.send(result);
+        });
+
+
+        // STIPE POST REQUEST
+        app.post("/create-payment-intent", async (req, res) =>{
+
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            });
+
         });
         
     } finally {
